a8c2113810355e9d3b921ce0463844859d73e658
yuuya1015 <151980934+yuuya1015@users.noreply.github.com>
2023-12-29 20:56:53 +0900

    修正２

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     first_name            { '試験' }
     first_name_ruby       { 'シケン' }
